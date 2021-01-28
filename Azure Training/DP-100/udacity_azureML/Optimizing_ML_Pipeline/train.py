31ea6c6aa6b6c7953fc55aa2d17b31f1501af151
Jane Pham <31594160+tramchip@users.noreply.github.com>
2021-01-29 01:51:37 +0700

    Add files via upload

---
+++ Azure Training/DP-100/udacity_azureML/Optimizing_ML_Pipeline/train.py
@@ -0,0 +1,98 @@
+from sklearn.linear_model import LogisticRegression
+import argparse
+import os
+import numpy as np
+from sklearn.metrics import mean_squared_error
+import joblib
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder
+import pandas as pd
+from azureml.core.run import Run
+from azureml.data.dataset_factory import TabularDatasetFactory
+from azureml.core import Workspace, Dataset
+
+# os.makedirs('./outputs', exist_ok=True)
+# joblib.dump(value=model, './outputs/model.joblib')
+# TODO: Create TabularDataset using TabularDatasetFactory
+# Data is located at:
+# "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
+
+
+
+### YOUR CODE HERE ###
+
+
+# TODO: Split data into train and test sets.
+
+### YOUR CODE HERE ###a
+
+
+def clean_data(data):
+    from azureml.data.dataset_factory import TabularDatasetFactory
+
+    # Dict for cleaning data
+    months = {"jan":1, "feb":2, "mar":3, "apr":4, "may":5, "jun":6, "jul":7, "aug":8, "sep":9, "oct":10, "nov":11, "dec":12}
+    weekdays = {"mon":1, "tue":2, "wed":3, "thu":4, "fri":5, "sat":6, "sun":7}
+
+    # Clean and one hot encode data
+    
+    x_df = data.to_pandas_dataframe().dropna()
+    jobs = pd.get_dummies(x_df.job, prefix="job")
+    x_df.drop("job", inplace=True, axis=1)
+    x_df = x_df.join(jobs)
+    x_df["marital"] = x_df.marital.apply(lambda s: 1 if s == "married" else 0)
+    x_df["default"] = x_df.default.apply(lambda s: 1 if s == "yes" else 0)
+    x_df["housing"] = x_df.housing.apply(lambda s: 1 if s == "yes" else 0)
+    x_df["loan"] = x_df.loan.apply(lambda s: 1 if s == "yes" else 0)
+    contact = pd.get_dummies(x_df.contact, prefix="contact")
+    x_df.drop("contact", inplace=True, axis=1)
+    x_df = x_df.join(contact)
+    education = pd.get_dummies(x_df.education, prefix="education")
+    x_df.drop("education", inplace=True, axis=1)
+    x_df = x_df.join(education)
+    x_df["month"] = x_df.month.map(months)
+    x_df["day_of_week"] = x_df.day_of_week.map(weekdays)
+    x_df["poutcome"] = x_df.poutcome.apply(lambda s: 1 if s == "success" else 0)
+
+    y_df = x_df.pop("y").apply(lambda s: 1 if s == "yes" else 0)
+    return x_df, y_df
+
+
+
+def main():
+    # Add arguments to script
+#     workspace_name =  'quick-starts-ws-136297'
+#     subscription_id = '610d6e37-4747-4a20-80eb-3aad70a55f43'
+#     resource_group =  ' aml-quickstarts-136297'
+#     workspace = Workspace(subscription_id, resource_group, workspace_name)
+
+
+    path = 'https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv'
+    ds = Dataset.Tabular.from_delimited_files(path=path)
+    x, y = clean_data(ds)
+    x_train, x_test, y_train, y_test = train_test_split(x,y, test_size = 0.2)
+    run = Run.get_context()
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--C', type=float, default=1.0, help="Inverse of regularization strength. Smaller values cause stronger regularization")
+    parser.add_argument('--max_iter', type=int, default=100, help="Maximum number of iterations to converge")
+
+    args = parser.parse_args()
+
+    run.log("Regularization Strength:", np.float(args.C))
+    run.log("Max iterations:", np.int(args.max_iter))
+
+    model = LogisticRegression(C=args.C, max_iter=args.max_iter).fit(x_train, y_train)
+
+    accuracy = model.score(x_test, y_test)
+    run.log("accuracy", np.float(accuracy))
+    
+    filename = "bankmarketing_model.pkl"
+    output_dir='./outputs/model'
+    os.makedirs(output_dir, exist_ok=True)
+    output_path = os.path.join(output_dir, filename)
+    joblib.dump(value=model, filename=output_path)
+    print("model saved in {}".format(output_path))
+
+if __name__ == '__main__':
+    main()
